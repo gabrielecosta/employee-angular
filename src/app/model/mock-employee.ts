f73db0dab1fb22ea62909551a17578844e8341fb
gabrielecosta <67752197+gabrielecosta@users.noreply.github.com>
2020-07-07 11:52:31 +0200

    Add interface Employee and mock-Emplyees

---
+++ src/app/model/mock-employee.ts
@@ -0,0 +1,22 @@
+import { Employee } from './employee';
+
+export const EMPLOYEES: Employee[] = [
+    {
+        id : 1,
+        name : "Mario",
+        surname : "Rossi",
+        active : true
+    },
+    {
+        id : 2,
+        name : "Giuseppe",
+        surname : "Verdi",
+        active : false
+    },
+    {
+        id : 3,
+        name : "Gabriele",
+        surname : "Costa",
+        active : true
+    }
+];
